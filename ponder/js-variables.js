7e0c269379d90f41039ed4da05cdcf15fca925be
jacob-strong <104380832+jacob-strong@users.noreply.github.com>
2026-09-17 11:14:54 -0600

    existence

--- ponder/js-variables.js
+++ ponder/js-variables.js
@@ -8,3 +8,19 @@ age = 34;
 
 console.log(age);
 
+const eyeColor = "brown";
+let favoriteColor = "blue";
+
+// scope is where variables can be referenced
+if (age == 34) {
+    console.log(name);
+
+    // inner scope variable
+    favoriteColor = "red";
+}
+
+// favoriteColor is not accessible outside of the if statement
+// unless it is declared outside
+console.log(favoriteColor);
+
+document.querySelector("h1").style.color = favoriteColor;
